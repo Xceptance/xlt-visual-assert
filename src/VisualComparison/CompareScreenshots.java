f351626241c6e8224475decb08378914078266c4
Damian Jordanov <d.jordanov@xceptance.net>
2015-09-01 17:29:22 +0200

    changed xlt-property path/names

--- src/VisualComparison/CompareScreenshots.java
+++ src/VisualComparison/CompareScreenshots.java
@@ -25,6 +25,7 @@ import com.xceptance.xlt.api.util.XltProperties;
  */
 public class CompareScreenshots implements WebDriverCustomModule
 {
+	final String PPREFIX = "com.xceptance.xlt.imageComparison";
     /**
      * {@inheritDoc}
      */
@@ -40,22 +41,21 @@ public class CompareScreenshots implements WebDriverCustomModule
 		}
 		
 		XltProperties x = XltProperties.getInstance();
-		
+			
 		//Checks if this is a new testcase. If yes, (re)sets index. If not. increments index
-		if ( (x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.currentID") == Session.getCurrent().getID()) ) {
-			String indexString = x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.index");
+		if ( (x.getProperty(PPREFIX + ".currentID") == Session.getCurrent().getID()) ) {
+			String indexString = x.getProperty(PPREFIX + ".index");
 			int indexInt = Integer.parseInt(indexString);
 			indexInt++;
 			indexString = String.valueOf(indexInt);
-			x.setProperty("com.xceptance.xlt.loadtests.TestCaseCP.index", indexString);
+			x.setProperty(PPREFIX + ".index", indexString);
 		}
 		
 		else { 
 			String currentID = Session.getCurrent().getID(); 
-			x.setProperty("com.xceptance.xlt.loadtests.TestCaseCP.currentID", currentID);
-			x.setProperty("com.xceptance.xlt.loadtests.TestCaseCP.index", "1");
+			x.setProperty(PPREFIX + ".currentID", currentID);
+			x.setProperty(PPREFIX + ".index", "1");
 		}
-		
 		
 		//Get testcasename for the correct folder
 		String currentTestCaseName = Session.getCurrent().getUserID();
@@ -65,12 +65,12 @@ public class CompareScreenshots implements WebDriverCustomModule
         String browserName = getBrowserName(webDriver);
                 	
     	//Get path to the directory
-        String directory = x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.directoryToScreenshots");
+        String directory = x.getProperty(PPREFIX + ".directoryToScreenshots");
         directory = directory + "/" + currentTestCaseName + "/" + browserName;
         
 
 		//Set name of the referenceImage       
-        String indexS = x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.index");
+        String indexS = x.getProperty(PPREFIX + ".index");
         String screenshotName = indexS + "-" + currentActionName;
         String referencePath = directory + "/" + screenshotName;
         
@@ -102,9 +102,9 @@ public class CompareScreenshots implements WebDriverCustomModule
             }
            
             //Get fuzzyness properties and convert them from String to integer/double
-            int pixelPerBlockX = Integer.parseInt(x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.pixelPerBlockX"));
-            int pixelPerBlockY = Integer.parseInt(x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.pixelPerBlockY"));
-            double threshold = Double.parseDouble(x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.threshold"));   
+            int pixelPerBlockX = Integer.parseInt(x.getProperty(PPREFIX + ".pixelPerBlockX"));
+            int pixelPerBlockY = Integer.parseInt(x.getProperty(PPREFIX + ".pixelPerBlockY"));
+            double threshold = Double.parseDouble(x.getProperty(PPREFIX + ".threshold"));   
             
             try {
             	//Initialize referenceImage, screenshotImage, delete screenshotImageFile
@@ -122,7 +122,7 @@ public class CompareScreenshots implements WebDriverCustomModule
             	File maskImageFile = new File(maskImagePath);
             	
             	//Initializes boolean variable for training mode
-            	String trainingModeString = x.getProperty("com.xceptance.xlt.loadtests.TestCaseCP.trainingMode");
+            	String trainingModeString = x.getProperty(PPREFIX + ".trainingMode");
             	Boolean trainingMode = Boolean.parseBoolean(trainingModeString);
             	
 
