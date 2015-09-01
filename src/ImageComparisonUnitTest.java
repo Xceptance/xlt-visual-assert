f89fd1252a7c7b1460b0bf070f9a1694da43ca14
Lucas <lzoulkowski@xceptance.net>
2015-09-01 08:40:34 +0200

    changed UnitTests

--- src/ImageComparisonUnitTest.java
+++ src/ImageComparisonUnitTest.java
@@ -19,81 +19,90 @@ public class ImageComparisonUnitTest {
 	
 	@Before 
 	public void setup() {
+//		sets up a new reference image for each test
 		reference = new BufferedImage(8, 10, BufferedImage.TYPE_INT_ARGB);
 		paintBlack(reference);
 	}
 
 	@Test
 	public void referenceMoreWidth()  throws IOException{
+//		reference image is wider than the new screenshots
 		x = 0;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (6, 10, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test
 	public void referenceLessWidth() throws IOException{
+//		reference image is less wide than the new screenshot
 		x = 1;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (10, 10, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test
 	public void referenceMoreHeight() throws IOException{
+//		reference image is higher than the new screenshot
 		x = 2;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (8, 8, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test
 	public void referenceLessHeight() throws IOException{
+//		reference image is less high than the new screenshot
 		x = 3;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (8, 12, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test
 	public void referenceBothMore() throws IOException{
+//		reference picture is wider and higher
 		x = 4;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (6, 8, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test 
 	public void referenceBothLess() throws IOException{
+//		new screenshot is wider and higher
 		x = 5;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (10, 12, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
 	@Test
 	public void referenceBroadFormat() throws IOException{
+//		width and height of the pictures are switched
 		x = 6;
 		outPutfile = new File("test"+x+".png");
 		maskFile = new File("mask"+x+".png");
 		newImage = new BufferedImage (10, 8, BufferedImage.TYPE_INT_ARGB);
-		paintWhite(newImage);
+		paintBlack(newImage);
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 
 	public void paintBlack (BufferedImage img) {
+//		method for painting the images black
 		for (int x = 0; x<img.getWidth(); x++) {
 			for (int y = 0; y<img.getHeight(); y++) {
 				img.setRGB(x, y, 0);
@@ -102,6 +111,7 @@ public class ImageComparisonUnitTest {
 	}
 	
 	public void paintWhite (BufferedImage img) {
+//		method for painting the images white
 		for (int x = 0; x<img.getWidth(); x++) {
 			for (int y = 0; y<img.getHeight(); y++) {
 				img.setRGB(x, y, Color.WHITE.getRGB());
