006484abb3ae4b1ec82c7578f3f19162ae9de315
Lucas <lzoulkowski@xceptance.net>
2015-09-02 11:22:40 +0200

    updated unit tests for TrainingMode and AdaptSize

--- src/VisualComparison/TimageComparison/TImageComparisonMarkSizes.java
+++ src/VisualComparison/TimageComparison/TImageComparisonMarkSizes.java
@@ -8,102 +8,80 @@ import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import VisualComparison.ImageComparison;
 
-
+//tests the correct behavior of ImageComparison for differently sized images
 public class TImageComparisonMarkSizes {
-//	tests the correct behavior of ImageComparison for differently sized images
-	BufferedImage reference, newImage;
+	static BufferedImage reference, slim, wide, low, high, slimAndLow, wideAndHigh, switched;
 	ImageComparison imgCompare = new ImageComparison(2, 2, 0.00, false);
-	int x;
-	File maskFile;
-	File outPutfile;
+	File directory = org.apache.commons.lang3.SystemUtils.getJavaIoTmpDir();
+	File outPutfile = new File(directory+"/test.png");
+	File maskFile = new File(directory+"/mask.png");
 	
-	@Before 
-	public void setup() {
-//		sets up a new reference image for each test
+//	sets up the images for the test
+	@BeforeClass 
+	public static void setup() {
 		reference = new BufferedImage(8, 10, BufferedImage.TYPE_INT_ARGB);
 		paintBlack(reference);
+		slim = new BufferedImage(6, 10, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(slim);
+		wide = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(wide);
+		low = new BufferedImage (8, 8, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(low);
+		high = new BufferedImage (8, 12, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(high);
+		slimAndLow = new BufferedImage (6, 8, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(slimAndLow);
+		wideAndHigh = new BufferedImage (10, 12, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(wideAndHigh);
+		switched = new BufferedImage (10, 8, BufferedImage.TYPE_INT_ARGB);
+		paintBlack(switched);
 	}
 
+//	reference image is wider than the new screenshots
 	@Test
 	public void referenceMoreWidth()  throws IOException{
-//		reference image is wider than the new screenshots
-		x = 0;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (6, 10, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, slim, maskFile, outPutfile));
 	}
 	
+//	reference image is less wide than the new screenshot
 	@Test
 	public void referenceLessWidth() throws IOException{
-//		reference image is less wide than the new screenshot
-		x = 1;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (10, 10, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, wide, maskFile, outPutfile));
 	}
-	
+
+//	reference image is higher than the new screenshot
 	@Test
 	public void referenceMoreHeight() throws IOException{
-//		reference image is higher than the new screenshot
-		x = 2;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (8, 8, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, low, maskFile, outPutfile));
 	}
-	
+
+//	reference image is less high than the new screenshot
 	@Test
 	public void referenceLessHeight() throws IOException{
-//		reference image is less high than the new screenshot
-		x = 3;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (8, 12, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, high, maskFile, outPutfile));
 	}
 	
+//	reference picture is wider and higher
 	@Test
 	public void referenceBothMore() throws IOException{
-//		reference picture is wider and higher
-		x = 4;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (6, 8, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, slimAndLow, maskFile, outPutfile));
 	}
-	
+
+//	new screenshot is wider and higher
 	@Test 
 	public void referenceBothLess() throws IOException{
-//		new screenshot is wider and higher
-		x = 5;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (10, 12, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, wideAndHigh, maskFile, outPutfile));
 	}
-	
+
+//	width and height of the pictures are switched
 	@Test
 	public void referenceBroadFormat() throws IOException{
-//		width and height of the pictures are switched
-		x = 6;
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage (10, 8, BufferedImage.TYPE_INT_ARGB);
-		paintBlack(newImage);
-		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
+		Assert.assertFalse(imgCompare.fuzzyEqual(reference, switched, maskFile, outPutfile));
 	}
 	
 	@After
@@ -111,9 +89,9 @@ public class TImageComparisonMarkSizes {
 		outPutfile.delete();
 		maskFile.delete();
 	}
-
-	public void paintBlack (BufferedImage img) {
-//		method for painting the images black
+	
+//	method for painting the images black
+	public static void paintBlack (BufferedImage img) {
 		int rgb = Color.BLACK.getRGB();
 		for (int x = 0; x<img.getWidth(); x++) {
 			for (int y = 0; y<img.getHeight(); y++) {
@@ -121,9 +99,9 @@ public class TImageComparisonMarkSizes {
 			}
 		}
 	}
-	
-	public void paintWhite (BufferedImage img) {
-//		method for painting the images white
+
+//	method for painting the images white
+	public static void paintWhite (BufferedImage img) {
 		int rgb = Color.WHITE.getRGB();
 		for (int x = 0; x<img.getWidth(); x++) {
 			for (int y = 0; y<img.getHeight(); y++) {
--- src/VisualComparison/TimageComparison/TImageComparisonTraining.java
+++ src/VisualComparison/TimageComparison/TImageComparisonTraining.java
@@ -7,59 +7,57 @@ import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import VisualComparison.ImageComparison;
 
 public class TImageComparisonTraining {
-	BufferedImage reference, newImage;
+	static BufferedImage reference, newImage;
 	ImageComparison training = new ImageComparison (10, 10, 0.00, true);
 	ImageComparison imgCompare = new ImageComparison (10, 10, 0.00, false);
-	int x;
-	File maskFile;
-	File outPutfile;
+	File directory = org.apache.commons.lang3.SystemUtils.getJavaIoTmpDir();
+	File outPutfile = new File(directory+"/test.png");
+	File maskFile = new File(directory+"/mask.png");
+
 	
-	@Before
-	public void setUp () {
+	@BeforeClass
+	public static void setUp () {
 		reference = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
 		paintWhite(reference);
+		newImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
 	}
 	
+//	test a run with the training mode without any differences between the images
 	@Test
 	public void trainWithNoDifference() throws IOException{
-//		test a run with the training mode without any differences between the images
-		x = 0;
 		setUpFileAndPicture();
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(training.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
+//	a single block is colored different and should be recognized by the training mode
 	@Test
 	public void trainWithSingleDifference() throws IOException{
-//		a single block is colored different and should be recognized by the training mode
-		x = 1;
 		setUpFileAndPicture();
 		paintArea(newImage, 0, 0, 10, 10);
 		fuzzyAssertBlock();
 	}
 	
+//	two areas are colored different and should be recognized by the training mode
 	@Test
 	public void trainWithMultipleDifferences() throws IOException{
-//		two areas are colored different and should be recognized by the training mode
-		x = 2;
 		setUpFileAndPicture();
 		paintArea(newImage, 50, 30, 23, 27);
 		paintArea(newImage, 78, 83, 10, 45);
 		fuzzyAssertBlock();
 	}
 	
+//	after a first round in training mode, additional differences are created to see if the training works
+//	properly over multiple runs
 	@Test
 	public void trainOverMultipleRounds() throws IOException{
-//		after a first round in training mode, additional differences are created to see if the training works
-//		properly over multiple runs
-		x = 3;
 		setUpFileAndPicture();
 		paintArea(newImage, 0, 0, 10, 10);
 		fuzzyAssertBlock();
@@ -67,10 +65,9 @@ public class TImageComparisonTraining {
 		fuzzyAssertBlock();
 	}
 	
+//	test if training mode works properly for the exact pixel comparison
 	@Test
 	public void trainExactlyEqual() throws IOException {
-//		test if training mode works properly for the exact pixel comparison
-		x = 4;
 		setUpFileAndPicture();
 		paintArea(newImage, 80, 80, 1, 1);
 		exactlyAssertBlock();
@@ -78,10 +75,9 @@ public class TImageComparisonTraining {
 		exactlyAssertBlock();
 	}
 	
+//	test if training mode works properly for the fuzzy pixel comparison
 	@Test
 	public void trainPixelFuzzyEqual() throws IOException {
-//		test if training mode works properly for the fuzzy pixel comparison
-		x = 5;
 		setUpFileAndPicture();
 		paintArea(newImage, 48, 97, 3, 2);
 		pixelFuzzyAssertBlock();		
@@ -92,9 +88,9 @@ public class TImageComparisonTraining {
 		outPutfile.delete();
 		maskFile.delete();
 	}
-		
-	public void paintWhite (BufferedImage img) {
-//		method for painting the images white
+	
+//	method for painting the images white
+	public  static void  paintWhite (BufferedImage img) {
 		int rgb = Color.WHITE.getRGB();
 		for (int x = 0; x<img.getWidth(); x++) {
 			for (int y = 0; y<img.getHeight(); y++) {
@@ -103,8 +99,8 @@ public class TImageComparisonTraining {
 		}
 	}
 	
+//	method for coloring specific areas of an image to create differences
 	public void paintArea (BufferedImage img, int x, int y, int width, int height) {
-//		method for coloring specific areas of an image to create differences
 		int rgb = Color.BLUE.getRGB();
 		for (int a = 0; a<width; a++){
 			for (int b = 0; b<height; b++) {
@@ -112,33 +108,30 @@ public class TImageComparisonTraining {
 			}
 		}
 	}
-	
+
+//	this assertion block checks if the fuzzy training was completed succesfully
 	public void fuzzyAssertBlock () throws IOException {
-//		this assertion block checks if the fuzzy training was completed succesfully
 		Assert.assertFalse(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(training.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(imgCompare.fuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
+//	this assertion block checks if the exact training was completed succesfully
 	public void exactlyAssertBlock() throws IOException {
-//		this assertion block checks if the exact training was completed succesfully
 		Assert.assertFalse(imgCompare.exactlyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(training.exactlyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(imgCompare.exactlyEqual(reference, newImage, maskFile, outPutfile));
 	}
 	
+//	this assertion block checks if the pixel fuzzy training was completed succesfully
 	public void pixelFuzzyAssertBlock() throws IOException {
-//		this assertion block checks if the pixel fuzzy training was completed succesfully
 		Assert.assertFalse(imgCompare.pixelFuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(training.pixelFuzzyEqual(reference, newImage, maskFile, outPutfile));
 		Assert.assertTrue(imgCompare.pixelFuzzyEqual(reference, newImage, maskFile, outPutfile));
 	}
-	
+
+//	setup method for each test
 	public void setUpFileAndPicture() {
-//		setup method for each test
-		outPutfile = new File("test"+x+".png");
-		maskFile = new File("mask"+x+".png");
-		newImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
 		paintWhite(newImage);
 	}
 
