01dc4a54b7e8d37e6c234f8e04bc1d0cfe4019b1
Damian Jordanov <d.jordanov@xceptance.net>
2015-09-16 08:44:04 +0200

    the test case for testing different browser versions and operating systems

---
+++ Columbia/Columbia.java
@@ -0,0 +1,15 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("Columbia")
+public class Columbia extends AbstractScriptTestCase
+{
+}
