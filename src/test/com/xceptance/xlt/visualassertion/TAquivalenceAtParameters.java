c21132d21256d516506933f447c67a4154e6616d
Rene Schwietzke <Rene Schwietzke>
2015-10-04 12:57:11 -0400

    Rework of a test class to understand it better... just the first set of verifications and reworks

--- src/test/com/xceptance/xlt/visualassertion/TAquivalenceAtParameters.java
+++ src/test/com/xceptance/xlt/visualassertion/TAquivalenceAtParameters.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -23,50 +24,60 @@ import com.xceptance.xlt.visualassertion.ImageComparison;
  * @author daniel
  * 
  */
-public class TAquivalenceAtParameters {
-
+public class TAquivalenceAtParameters
+{
 	private static BufferedImage reference;
 	private static BufferedImage screenshot;
 
-	private final static File directory = SystemUtils.getJavaIoTmpDir();
-	private static File fileMask = new File(directory, "/fileMask.png");
-	private static File fileOutExactly = new File(directory,
-			"/fileOutExactly.png");
-	private static File fileOutFuzzy = new File(directory,
-			"/fileOutExactly.png");
-	private static File differenceFileExactly = new File(directory,
-			"/difference.png");
-	private static File differenceFileFuzzy = new File(directory,
-			"/difference.png");
-	private static File thrashFile = new File(directory, "/tempFile");
+	private static File testDir;			
 
-	private static ImageComparison imgCompExact = new ImageComparison(10, 10, 0,
-			0, 0, false, false, 0, 0, true, "EXACTLY");
-	private static ImageComparison imgCompPFuzzy = new ImageComparison(10, 10, 0,
-			0, 0, false, false, 0, 0, true, "PIXELFUZZY");
-	private static ImageComparison imgCompFuzzy = new ImageComparison(10, 10, 1,
-			0, 0, false, false, 0, 0, true, "FUZZY");
+	private static File fileMask;
+	private static File fileOutExactly;
+	private static File fileOutFuzzy;
+	private static File differenceFileExactly;
+	private static File differenceFileFuzzy;
+	private static File thrashFile;
+
+	private static ImageComparison imgCompExact = new ImageComparison(10, 10, 0, 0, 0, false, false, 0, 0, true, "EXACTLY");
+	private static ImageComparison imgCompPFuzzy = new ImageComparison(10, 10, 0, 0, 0, false, false, 0, 0, true, "PIXELFUZZY");
+	private static ImageComparison imgCompFuzzy = new ImageComparison(10, 10, 1, 0, 0, false, false, 0, 0, true, "FUZZY");
 
 	/**
 	 * Initializes two 200 * 200 images, all pixels get a random number between
 	 * -16777216 and 0 as rgb value
+	 * @throws IOException 
 	 */
 	@BeforeClass
-	public static void initializeImages() {
+	public static void initializeImages() throws IOException
+	{
+		testDir = new File(SystemUtils.getJavaIoTmpDir(), TAquivalenceAtParameters.class.getSimpleName());
+		FileUtils.forceMkdir(testDir);
+
+		fileMask = new File(testDir, "fileMask.png");
+		fileOutExactly = new File(testDir, "fileOutExactly.png");
+		fileOutFuzzy = new File(testDir,	"fileOutExactly.png");
+		differenceFileExactly = new File(testDir, "difference.png");
+		differenceFileFuzzy = new File(testDir, "difference.png");
+		thrashFile = new File(testDir, "tempFile");
+
 		final Random random = new Random();
 
 		int rgb;
 		reference = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);
-		for (int w = 0; w < reference.getWidth(); w++) {
-			for (int h = 0; h < reference.getHeight(); h++) {
+		for (int w = 0; w < reference.getWidth(); w++)
+		{
+			for (int h = 0; h < reference.getHeight(); h++)
+			{
 				rgb = random.nextInt(16777216) * (-1);
 				reference.setRGB(w, h, rgb);
 			}
 		}
 
 		screenshot = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);
-		for (int w = 0; w < screenshot.getWidth(); w++) {
-			for (int h = 0; h < screenshot.getHeight(); h++) {
+		for (int w = 0; w < screenshot.getWidth(); w++)
+		{
+			for (int h = 0; h < screenshot.getHeight(); h++)
+			{
 				rgb = random.nextInt(16777216) * (-1);
 				screenshot.setRGB(w, h, rgb);
 			}
@@ -82,30 +93,28 @@ public class TAquivalenceAtParameters {
 	 * @throws IOException
 	 */
 	@Test
-	public void test() throws IOException {
-
-		// Compares FUZZY against EXACTLY 
-		imgCompExact.isEqual(reference, screenshot, fileMask, fileOutExactly,
-				differenceFileExactly);
-		imgCompFuzzy.isEqual(reference, screenshot, fileMask, fileOutFuzzy,
-				differenceFileFuzzy);
+	public void test() throws IOException
+	{
+		// Compares FUZZY against EXACTLY
+		imgCompExact.isEqual(reference, screenshot, fileMask, fileOutExactly, differenceFileExactly);
+		imgCompFuzzy.isEqual(reference, screenshot, fileMask, fileOutFuzzy,	differenceFileFuzzy);
 
 		BufferedImage reference = ImageIO.read(fileOutExactly);
 		BufferedImage screenshot = ImageIO.read(fileOutFuzzy);
 		Assert.assertTrue(imgCompExact.isEqual(reference, screenshot, fileMask, thrashFile, thrashFile));
+
 		reference = ImageIO.read(differenceFileExactly);
 		screenshot = ImageIO.read(differenceFileFuzzy);
 		Assert.assertTrue(imgCompExact.isEqual(reference, screenshot, fileMask, thrashFile, thrashFile));
 
-		// Compares PIXELFUZZY against EXACTLY 
-		imgCompExact.isEqual(reference, screenshot, fileMask, fileOutExactly,
-				differenceFileExactly);
-		imgCompPFuzzy.isEqual(reference, screenshot, fileMask, fileOutFuzzy,
-				differenceFileFuzzy);
+		// Compares PIXELFUZZY against EXACTLY
+		imgCompExact.isEqual(reference, screenshot, fileMask, fileOutExactly, differenceFileExactly);
+		imgCompPFuzzy.isEqual(reference, screenshot, fileMask, fileOutFuzzy, differenceFileFuzzy);
 
 		reference = ImageIO.read(fileOutExactly);
 		screenshot = ImageIO.read(fileOutFuzzy);
 		Assert.assertTrue(imgCompExact.isEqual(reference, screenshot, fileMask, thrashFile, thrashFile));
+
 		reference = ImageIO.read(differenceFileExactly);
 		screenshot = ImageIO.read(differenceFileFuzzy);
 		Assert.assertTrue(imgCompExact.isEqual(reference, screenshot, fileMask, thrashFile, thrashFile));
@@ -114,15 +123,12 @@ public class TAquivalenceAtParameters {
 
 	/**
 	 * Deletes the temporary files which were created for this test
+	 * @throws IOException 
 	 */
 	@AfterClass
-	public static void deleteFile() {
-		fileMask.delete();
-		fileOutExactly.delete();
-		fileOutFuzzy.delete();
-		differenceFileExactly.delete();
-		differenceFileFuzzy.delete();
-		thrashFile.delete();
+	public static void deleteFile() throws IOException
+	{
+		FileUtils.deleteDirectory(testDir);
 	}
 
 }
